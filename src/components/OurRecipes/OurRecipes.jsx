effc8356e05513ec5b61902cc7ffd5787f42f598
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-22 12:08:40 +0600

    title and description of our recipes section

--- src/components/OurRecipes/OurRecipes.jsx
+++ src/components/OurRecipes/OurRecipes.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const OurRecipes = () => {
     return (
-        <div>
-            <h3>Our Recipes</h3>
+        <div className='text-center my-8 w-8/12 mx-auto space-y-4'>
+            <h1 className='font-semibold text-5xl text-gray-800'>Our Recipes</h1>
+            <p className='text-base text-gray-600'>
+                Explore our curted recipes for culinary inspiration. From classic comfort to gourmet delights, we have got something to satisfy every craving. Elevate your cooking with our easy-to-follow recipes, perfect for any occasion
+            </p>
         </div>
     );
 };
