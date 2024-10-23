6ca175a75fe297c6f50487026f6b15bb0ae0fb65
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-23 12:43:38 +0600

    currently cooking section complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   const [recipeQueue, setRecipeQueue] = useState([])
+  // creating usestate for preparing section
+  const [preparedRecipe, setPreparedRecipe] = useState([])
+  const [totalTime, setTotalTime] = useState(0)
+  const [totalCalories, setTotalCalories] = useState(0)
 
   const addRecipeToQueue = (recipe) =>{
     // to check if the recipe already exist in the array
@@ -19,6 +23,19 @@ function App() {
     }
   }
 
+  const handleRemove = id =>{
+    //find which recipe to remove
+    const deletedRecipe = recipeQueue.find(recipe => recipe.recipe_id === id)
+    // remove from want to cook table
+    const updatedQueue = recipeQueue.filter(recipe => recipe.recipe_id !== id)
+    setRecipeQueue(updatedQueue)
+    setPreparedRecipe([...preparedRecipe, deletedRecipe])
+  }
+
+  const calculateTimeAndCalories = (time, calories) =>{
+      setTotalTime(totalTime + time)
+      setTotalCalories(totalCalories + calories)
+  }
   console.log(recipeQueue)
   return (
     <div className='w-11/12 mx-auto'>
@@ -30,7 +47,14 @@ function App() {
         {/* Cards section */}
         <Recipes addRecipeToQueue={addRecipeToQueue}></Recipes>
         {/* Sidebar */}
-        <Sidebar recipeQueue={recipeQueue}></Sidebar>
+        <Sidebar 
+        handleRemove={handleRemove} 
+        recipeQueue={recipeQueue} 
+        preparedRecipe={preparedRecipe}
+        calculateTimeAndCalories={calculateTimeAndCalories}
+        totalTime={totalTime}
+        totalCalories={totalCalories}
+        ></Sidebar>
       </section>
     </div>
   )
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-const Sidebar = ({ recipeQueue }) => {
+const Sidebar = ({ recipeQueue, handleRemove, preparedRecipe, calculateTimeAndCalories, totalTime, totalCalories }) => {
   return (
     <div className="md:w-1/3 border rounded-2xl text-gray-600 p-2 bg-base-100 ml-4 shadow-xl">
       {/* want to cook table */}
@@ -22,13 +22,47 @@ const Sidebar = ({ recipeQueue }) => {
                 <td>{recipe.recipe_name}</td>
                 <td>{recipe.preparing_time}</td>
                 <td>{recipe.calories}</td>
-                <td><button onClick={()=> console.log('preparing')} className="px-2 py-1 md:px-4 bg-green-400 rounded-full cursor-pointer hover:bg-green-500 transition-all font-bold">Preparing</button></td>
+
+                <td><button onClick={()=> {
+                    handleRemove(recipe.recipe_id)
+                    calculateTimeAndCalories(recipe.preparing_time, recipe.calories)} 
+                } className="px-2 py-1 md:px-4 bg-green-400 rounded-full cursor-pointer hover:bg-green-500 transition-all font-bold">Preparing</button></td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
       {/* currently cooking table */}
+      <div className="overflow-x-auto mt-8">
+        <h2 className="text-center border-b-2 py-2 font-bold text-2xl">Currently cooking: {preparedRecipe.length}</h2>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {preparedRecipe.map((recipe, idx) => (
+              <tr className="hover" key={idx}>
+                <th>{idx + 1}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+              </tr>
+            ))}
+            <tr className="border-none">
+            <th></th>
+                <td></td>
+                <td>Total time = {totalTime} minutes</td>
+                <td>Total calories = {totalCalories}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
